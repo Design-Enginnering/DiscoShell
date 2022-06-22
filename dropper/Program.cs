e338b201bd5d7c9eb7bdd447707636f657c79aae
chash <70506265+ch2sh@users.noreply.github.com>
2022-06-22 21:11:24 +0800

    Update Program.cs

--- dropper/Program.cs
+++ dropper/Program.cs
@@ -14,7 +14,7 @@ namespace dropper
 {
     internal class Program
     {
-        private static string token = "https://pastebin.com/raw/xs0Q8kzv";
+        private static string token = "";
         private static string prefix = "";
         private static string geolock = "";
         private static string spreadmessage = @"";
